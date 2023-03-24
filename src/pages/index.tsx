c3a33423c79e70b229cea559eb531e1b9ab09d73
Goblin <jonathonhawkins92@gmail.com>
2023-03-24 11:44:23 +0000

    Add posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-    const hello = api.example.hello.useQuery({ text: "from tRPC" });
+    const { data: posts = [] } = api.posts.getAll.useQuery();
     const { user } = useUser();
 
-    console.log(user);
     return (
         <>
             <Head>
@@ -20,6 +18,11 @@ const Home: NextPage = () => {
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 {!user && <SignInButton />}
                 {!!user && <SignOutButton />}
+                <ul>
+                    {posts.map((post) => (
+                        <li key={post.id}>{post.content}</li>
+                    ))}
+                </ul>
             </main>
         </>
     );
