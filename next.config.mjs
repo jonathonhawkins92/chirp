4b9c97ec99c3865afdba4bfd308ef3657205a7b3
Goblin <jonathonhawkins92@gmail.com>
2023-03-24 17:28:57 +0000

    Disable Vercel build error and swcminify

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,14 @@ const config = {
     images: {
         domains: ["images.clerk.dev"],
     },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    swcMinify: true,
 };
 export default config;
 
